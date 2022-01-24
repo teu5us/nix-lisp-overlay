1c7146ecd46772c5fb2fca75be670007e2fbd969
Pavel Stepanov <paulkreuzmann@gmail.com>
2022-01-24 15:01:10 +0600

    fix infinite recursion

--- pkgs/development/lisp-modules/setup-hook.sh
+++ pkgs/development/lisp-modules/setup-hook.sh
@@ -1,21 +1,30 @@
 # This setup hook adds every propagated lisp system to CL_SOURCE_REGISTRY
 
-buildAsdfPath ()  {
+buildAsdfPath () {
     declare -A lispPathsSeen=()
     for system in @lispInputs@; do
+        _addToAsdfPath $system
+    done
+}
+
+_addToAsdfPath ()  {
+    local system="$1"
+    if [ -v lispPathsSeen[$system] ]; then
+        return
+    else
+        lispPathsSeen[$system]=1
         local path="$system/lib/common-lisp//"
         if [ -d "$path" ]; then
-            lispPathsSeen[$path]=1
             addToSearchPath "CL_SOURCE_REGISTRY" "$path"
             local prop="$system/nix-support/propagated-build-inputs"
             if [ -e "$prop" ]; then
-                local new_path
+                local new_system
                 for new_system in $(cat $prop); do
-                    addToAsdfPath "$new_system"
+                    _addToAsdfPath "$new_system"
                 done
             fi
         fi
-    done
+    fi
 }
 
 addEnvHooks "$hostOffset" buildAsdfPath
