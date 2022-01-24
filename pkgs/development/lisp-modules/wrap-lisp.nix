afb75f4b9181bcc8b503d5584310f3301540ec97
Pavel Stepanov <paulkreuzmann@gmail.com>
2022-01-24 15:31:33 +0600

    don't link the entire compiiler output

--- pkgs/development/lisp-modules/wrap-lisp.nix
+++ pkgs/development/lisp-modules/wrap-lisp.nix
@@ -8,14 +8,31 @@ let
   lispInputs = f packageSet;
   asdfHook = (pkgs.callPackage ./setup-hook.nix {}) lispInputs;
 in
-symlinkJoin {
+stdenv.mkDerivation {
   name = "${compiler.name}-with-packages";
   nativeBuildInputs = [ makeWrapper asdfHook ];
-  buildInputs = [ asdf ];
-  paths = [ compiler ];
-  postBuild = ''
-    wrapProgram $out/bin/${compiler.pname} \
+  buildInputs = [ asdf compiler ];
+  src = compiler;
+  phases = [ "buildPhase" "installPhase" ];
+  buildPhase = ''
+    makeWrapper $src/bin/${compiler.pname} ./${compiler.pname} \
       --set "CL_SOURCE_REGISTRY" "$CL_SOURCE_REGISTRY" \
       --add-flags "--load \"${asdf}/lib/common-lisp/asdf/build/asdf.lisp\""
   '';
+  installPhase = ''
+    mkdir -p $out/bin
+    cp ${compiler.pname} $out/bin
+  '';
 }
+
+# symlinkJoin {
+#   name = "${compiler.name}-with-packages";
+#   nativeBuildInputs = [ makeWrapper asdfHook ];
+#   buildInputs = [ asdf ];
+#   paths = [ compiler ];
+#   postBuild = ''
+#     wrapProgram $out/bin/${compiler.pname} \
+#       --set "CL_SOURCE_REGISTRY" "$CL_SOURCE_REGISTRY" \
+#       --add-flags "--load \"${asdf}/lib/common-lisp/asdf/build/asdf.lisp\""
+#   '';
+# }
