a16d98a9e981da93c28fdb2d27f8561db90a5975
Pavel Stepanov <paulkreuzmann@gmail.com>
2022-01-27 10:34:44 +0600

    cleanup

--- pkgs/development/lisp-modules/resolve-lisp-inputs.nix
+++ pkgs/development/lisp-modules/resolve-lisp-inputs.nix
@@ -5,16 +5,20 @@ inputs:
 with builtins;
 
 let
-  required = lib.filterAttrs (n: v:
-    if (isAttrs v) && (hasAttr "providedSystems" v)
-      then
-        any (input:
-          elem input v.providedSystems
-        ) inputs
-      else
-        false
-  ) scope;
-  requiredSystems = concatMap (p: p.providedSystems) (attrValues required);
+  required = attrValues
+    (lib.filterAttrs
+      (n: v:
+        if
+          (isAttrs v) && (hasAttr "providedSystems" v)
+        then
+          any (input:
+            elem input v.providedSystems
+          ) inputs
+        else
+          false)
+      scope);
+  requiredSystems = concatMap (p: p.providedSystems) required;
+  allPackagesFound = all (input: elem input requiredSystems) inputs;
 in
-assert all (input: elem input requiredSystems) inputs;
-attrValues required
+assert allPackagesFound;
+required
