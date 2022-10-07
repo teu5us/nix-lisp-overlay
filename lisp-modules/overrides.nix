5786a527c430278a74515b65a9cbe678e742e6b9
Pavel Stepanov <paulkreuzmann@gmail.com>
2022-10-08 00:04:49 +0600

    4008 systems, new overrides

--- lisp-modules/overrides.nix
+++ lisp-modules/overrides.nix
@@ -9,11 +9,37 @@ scope.overrideScope' (self: super: {
     propagatedBuildInputs = oa.propagatedBuildInputs ++ [ pkgs.libuv ];
   });
 
+  cl_plus_ssl_merged = super.cl_plus_ssl_merged.overrideAttrs (oa: {
+    propagatedBuildInputs = oa.propagatedBuildInputs ++ [ pkgs.openssl.out ];
+  });
+
+  swank = super.swank.overrideAttrs (oa: {
+    extraFiles = [
+      "swank" "contrib" "lib"
+      "metering.lisp"
+      "nregex.lisp"
+      "packages.lisp"
+      "sbcl-pprint-patch.lisp"
+      "start-swank.lisp"
+      "swank-loader.lisp"
+      "swank.lisp"
+      "xref.lisp"
+    ];
+  });
+
   cl-async = super.cl-async.overrideAttrs (oa: {
     providedSystems = [ "cl-async" ];
   });
 
   static-vectors = super.static-vectors.overrideAttrs (oa: {
+    extraFiles = [ "version.sexp" ];
+  });
+
+  trivial-with-current-source-form = super.trivial-with-current-source-form.overrideAttrs (oa: {
+    extraFiles = [ "version-string.sexp" ];
+  });
+
+  usocket = super.usocket.overrideAttrs (oa: {
     extraFiles = [ "version.sexp" ];
   });
 
